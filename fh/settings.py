aae5b2d109cfa9cf82ca5f99bab5e091aa2f8253
Sergey Dubovskii <webmalc@gmail.com>
2015-06-23 19:25:33 +0300

    Users app

--- fh/settings.py
+++ fh/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'debug_toolbar',
     'django_extensions',
+    'users'
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ users/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+admin.ModelAdmin.change_list_template = "admin/change_list_filter_sidebar.html"
+
