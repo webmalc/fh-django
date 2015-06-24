63a2c2d338f7c5b39ee5ecb46c9c964b0dc5ef9b
Sergey Dubovskii <webmalc@gmail.com>
2015-06-24 13:55:38 +0300

    Finances payment admin

--- finances/admin.py
+++ finances/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from finances import models
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'amount', 'is_incoming', 'created_at', 'created_by', 'modified_at', 'modified_by')
+    list_display = ('id', 'amount', 'date', 'is_incoming', 'created_at', 'created_by', 'modified_at', 'modified_by')
     list_display_links = ('id', 'amount')
-    fields = ['amount', 'is_incoming', 'created_by']
+    list_filter = ['is_incoming', 'created_by', 'date', 'created_at']
+    fieldsets = [
+        (None, {'fields': ['amount', 'is_incoming']}),
+        ('Date & user', {'fields': ['date', 'created_by'], 'classes': ['collapse']}),
+    ]
+    ordering = ['-date']
 
 admin.site.register(models.Payment, PaymentAdmin)
--- finances/models.py
+++ finances/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
@@ -10,9 +11,11 @@ class Payment(CommonInfo):
     amount = models.DecimalField(max_digits=20, decimal_places=2,
                                  validators=[MinValueValidator(0.01)])
     is_incoming = models.BooleanField(default=False, verbose_name='Is incoming?')
+    date = models.DateTimeField(default=datetime.now())
 
     def __unicode__(self):
         return u'#%s' % self.id
 
-
+    class Meta:
+        ordering = ['-date']
 
