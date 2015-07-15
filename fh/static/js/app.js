499ce2db07f9280e977b5b3335b53ecae82106db
webmalc <webmalc@gmail.com>
2015-07-15 18:03:19 +0300

    Bootstrap switch

---
+++ fh/static/js/app.js
@@ -0,0 +1,12 @@
+/*jslint browser: true*/
+/*global $*/
+$(document).ready(function () {
+    'use strict'
+
+    //bootstrap switch
+    $('input[type="checkbox"]').bootstrapSwitch({
+        size: 'small',
+        onText: 'yes',
+        offText: 'no'
+    });
+});
