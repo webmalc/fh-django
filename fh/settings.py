1d394e22e2da74f5c665f70a43217863930d58dd
webmalc <webmalc@gmail.com>
2015-10-19 15:01:24 +0300

    Hide delete button on small resolutions

--- fh/settings.py
+++ fh/settings.py
@@ -25,6 +25,7 @@ AUTHENTICATION_BACKENDS = (
 INSTALLED_APPS = (
     'autocomplete_light',
     'suit',
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
